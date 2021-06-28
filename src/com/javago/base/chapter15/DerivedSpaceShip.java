61dfcd4fcaf1a8280d2b4a99500e1f1686f31c03
sophoraFlower <2268228773@qq.com>
2021-06-28 19:51:03 +0800

    OnJava8 复用 0628

---
+++ src/com/javago/base/chapter15/DerivedSpaceShip.java
@@ -0,0 +1,15 @@
+package com.javago.base.chapter15;
+
+public class DerivedSpaceShip extends SpaceShipControls{
+    private String name;
+    public DerivedSpaceShip(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() { return name; }
+    public static void main(String[] args) {
+        DerivedSpaceShip protector =
+                new DerivedSpaceShip("NSEA Protector");
+        protector.forward(100);
+    }
+}
