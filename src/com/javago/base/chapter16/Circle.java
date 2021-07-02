a396c578e0edb468c8376319780dd580385fa9a5
sophoraFlower <2268228773@qq.com>
2021-07-03 00:36:09 +0800

    OnJava8 多态-动态绑定 0703

---
+++ src/com/javago/base/chapter16/Circle.java
@@ -0,0 +1,12 @@
+package com.javago.base.chapter16;
+
+public class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle.draw()");
+    }
+    @Override
+    public void erase() {
+        System.out.println("Circle.erase()");
+    }
+}
---
+++ src/com/javago/base/chapter16/Shape.java
@@ -0,0 +1,6 @@
+package com.javago.base.chapter16;
+
+public class Shape {
+    public void draw() {}
+    public void erase() {}
+}
