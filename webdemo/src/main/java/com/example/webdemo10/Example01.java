15b797539509c78b67a630044a4be43217511db0
sophoraFlower <2268228773@qq.com>
2021-07-16 00:55:48 +0800

    JavaWeb JDBC 0716

---
+++ webdemo/src/main/java/com/example/webdemo10/Example01.java
@@ -0,0 +1,52 @@
+package com.example.webdemo10;
+
+import java.sql.*;
+
+public class Example01 {
+
+    public static void main(String[] args) throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        try {
+            // Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/jdbc";
+            String username = "root";
+            String password = "2021@Tokyo";
+            conn = DriverManager.getConnection(url, username, password);
+            stmt = conn.createStatement();
+            String sql = "select * from users";
+            rs = stmt.executeQuery(sql);
+            System.out.println("id  |  name  | password  |  birthday");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String psw = rs.getString("password");
+                Date birthday = rs.getDate("birthday");
+                System.out.println(id + "  |   " + name + "  |  " + password + "  |  " + birthday);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                rs = null;
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                conn = null;
+            }
+        }
+    }
+
+}
