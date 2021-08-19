08e895324373859f2df0689462ae6c66c168b9c9
sophoraFlower <2268228773@qq.com>
2021-08-19 23:28:34 +0800

    JavaWeb getRequestDispatcher/EL 表达式 0819

---
+++ webdemo/src/main/java/com/basis/javaweb/web04/User.java
@@ -0,0 +1,31 @@
+package com.basis.javaweb.web04;
+
+public class User {
+    private int id;
+    private String name;
+    private String host;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
