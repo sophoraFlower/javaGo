b181cc18f89d429da69142319387e9565ba90839
HouleX <houle.cf@foxmail.com>
2021-12-12 22:54:53 +0800

    Create NoteBook.java
    Java OOP practice 2021/12/12

---
+++ object-oriented-programming/ObjectContainer/NoteBook.java
@@ -0,0 +1,29 @@
+package ObjectContainer;
+
+public class NoteBook {
+
+    public void add(String s){
+
+    }
+
+    public int getSize(){
+        return 0;
+    }
+
+    public String getNote(){
+        return "";
+    }
+    
+    public boolean removeNote(int index){
+        return true;
+    }
+
+    public String[] list(){
+        return new String[10];
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
