5a04205803d5607089e1ed19d9e3a6bf0272c79a
sophoraFlower <2268228773@qq.com>
2021-12-21 19:24:58 +0800

    Java OOP practice 2021/12/21
    Java OOP practice 2021/12/21

---
+++ mirai-kkpm/mirai-kkpm-api-csi-core-if-lib/src/main/java/jp/co/sjnk/mirai/kk/kkpm/csi/business/facade/online/dto/package-info.java
@@ -0,0 +1,5 @@
+(c)2017 Sompo Japan Nipponkoa Insurance Inc.
+/**
+* BBBBBBBBBBBBBBBBBBBBB.
+*/
+package jp.co.sjnk.mirai.kk.kkpm.csi.business.facade.online.dto;
---
+++ mirai-kkpm/mirai-kkpm-api-csi-core-if-lib/src/main/java/jp/co/sjnk/mirai/kk/kkpm/csi/business/facade/online/package-info.java
@@ -0,0 +1,5 @@
+(c)2017 Sompo Japan Nipponkoa Insurance Inc.
+/**
+* AAAAAAAAAAAAAAAAAAAAAA.
+*/
+package jp.co.sjnk.mirai.kk.kkpm.csi.business.facade.online;
