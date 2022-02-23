3090ab40f9cc8d52a7da0484ecc01885c08ff69e
Houle <houle.cf@foxmail.com>
2022-02-23 14:06:00 +0800

    SpringBoot、Swagger 2022/02/23

--- api-auto-test/Chapter10/service/MyGetMethod.java
+++ api-auto-test/Chapter10/service/MyGetMethod.java
@@ -1,7 +1,7 @@
 package Chapter10.service;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
@@ -11,29 +11,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@SpringBootConfiguration
 @RestController
+@Api(value = "/",description = "这是我全部的get方法")
 public class MyGetMethod {
 
-    @RequestMapping(value = "/getCookies", method = RequestMethod.GET)
+    @RequestMapping(value = "/getCookies",method = RequestMethod.GET)
+    @ApiOperation(value = "通过这个方法可以获取到Cookies",httpMethod = "GET")
     public String getCookies(HttpServletResponse response){
-        Cookie cookie = new Cookie("login", "true");
+        //HttpServerletRequest 装请求信息的类
+        //HttpServerletResponse  装响应信息的类
+        Cookie cookie = new Cookie("login","true");
         response.addCookie(cookie);
-        return "恭喜你获得cookies信息成功～";
+        return "恭喜你获得cookies信息成功";
     }
 
-    @RequestMapping(value = "/get/with/cookies", method = RequestMethod.GET)
+    /**
+     * 要求客户端携带cookies访问
+     * 这是一个需要携带cookies信息才能访问的get请求
+     */
+    @RequestMapping(value = "/get/with/cookies",method = RequestMethod.GET)
+    @ApiOperation(value = "要求客户端携带cookies访问",httpMethod = "GET")
     public String getWithCookies(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
         if(Objects.isNull(cookies)){
-            return "该get请求必须携带cookies";
+            return "你必须携带cookies信息来";
         }
         for(Cookie cookie : cookies){
-            if(cookie.getName().equals("login") && cookie.getValue().equals("true")){
-                return "恭喜你访问成功～";
+            if(cookie.getName().equals("login") &&
+                    cookie.getValue().equals("true")){
+                return "这是一个需要携带cookies信息才能访问的get请求!";
             }
         }
-        return "该get请求必须携带cookies";
+
+        return "你必须携带cookies信息来";
     }
 
     /**
@@ -43,8 +53,7 @@ public class MyGetMethod {
      */
     @RequestMapping(value = "/get/with/param",method = RequestMethod.GET)
     @ApiOperation(value = "需求携带参数才能访问的get请求方法一",httpMethod = "GET")
-    public Map<String,Integer> getList(@RequestParam Integer start,
-                                       @RequestParam Integer end){
+    public Map<String,Integer> getList(@RequestParam Integer start, @RequestParam Integer end){
         Map<String,Integer> myList = new HashMap<>();
         myList.put("鞋",400);
         myList.put("干脆面",1);
@@ -58,14 +67,11 @@ public class MyGetMethod {
      */
     @RequestMapping(value = "/get/with/param/{start}/{end}")
     @ApiOperation(value = "需求携带参数才能访问的get请求的第二种实现",httpMethod = "GET")
-    public  Map myGetList(@PathVariable Integer start,
-                          @PathVariable Integer end){
+    public Map myGetList(@PathVariable Integer start, @PathVariable Integer end){
         Map<String,Integer> myList = new HashMap<>();
         myList.put("鞋",400);
         myList.put("干脆面",1);
         myList.put("衬衫",300);
         return  myList;
     }
-
 }
-
