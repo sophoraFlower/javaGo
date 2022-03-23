a759a570b25be89feda31f5381e3b98bbddb3a8f
Houle <houle.cf@foxmail.com>
2022-03-23 12:39:43 +0800

    Java base 2022/03/23

---
+++ coder-x/code007/todo02/RunLittleSupperMarketAppMain.java
@@ -0,0 +1,15 @@
+package code007.todo02;
+
+import code007.todo02.supermarket.LittleSuperMarket;
+import code007.todo02.supermarket.MerchandiseV2;
+
+public class RunLittleSupperMarketAppMain {
+    public static void main(String[] args) {
+        // 创建一个小超市类
+        LittleSuperMarket littleSuperMarket = new LittleSuperMarket(
+            "有家小超市", "浦东新区世纪大道666号",
+            100, 200, 200);
+
+        System.out.println("VIP的折上折折扣最终为：" + MerchandiseV2.getDiscountOnDiscount(littleSuperMarket));
+    }
+}
