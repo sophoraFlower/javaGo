5e0ebc1a61971920a03207ce5c5d95a96d891727
Houle <houle.cf@foxmail.com>
2022-03-26 00:25:14 +0800

    delete unnecessary project 2022/03/26

--- mirai-kkpm/mirai-kkpm-api-csi-core-if-lib/src/main/java/jp/co/sjnk/mirai/kk/kkpm/csi/business/facade/online/dto/package-info.java
+++
@@ -1,5 +0,0 @@
-(c)2017 Sompo Japan Nipponkoa Insurance Inc.
-/**
-* BBBBBBBBBBBBBBBBBBBBB.
-*/
-package jp.co.sjnk.mirai.kk.kkpm.csi.business.facade.online.dto;
--- mirai-kkpm/mirai-kkpm-api-csi-core-if-lib/src/main/java/jp/co/sjnk/mirai/kk/kkpm/csi/business/facade/online/impl/package-info.java
+++
@@ -1,5 +0,0 @@
-(c)2017 Sompo Japan Nipponkoa Insurance Inc.
-/**
-* XXXXXXXXXXXXXXXXXXXXX.
-*/
-package jp.co.sjnk.mirai.kk.kkpm.csi.business.facade.online.impl;
