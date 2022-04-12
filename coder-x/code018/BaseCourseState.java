15746794ad3cb4f73ece3715d43179bb4ad66b26
Houle <houle.cf@foxmail.com>
2022-04-12 23:37:31 +0800

    Java Foundation 2022/04/12

---
+++ coder-x/code018/BaseCourseState.java
@@ -0,0 +1,11 @@
+package code018;
+
+public abstract class BaseCourseState {
+
+    public static final int NEW_COURSE = 1;
+    public static final int UNAUTHED_COURSE = 2;
+    public static final int PASSED_COURSE = 3;
+    public static final int NOT_PASSED_COURSE = 4;
+    public static final int DELETED_COURSE = 5;
+
+}
