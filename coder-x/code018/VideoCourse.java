a6a0e2ec7d1b69a7764fb3345c5aaf30cf4c133b
Houle <houle.cf@foxmail.com>
2022-04-12 23:38:25 +0800

    Java Foundation 2022/04/12

---
+++ coder-x/code018/VideoCourse.java
@@ -0,0 +1,23 @@
+package code018;
+
+public class VideoCourse {
+
+    private String videoName;
+    private Integer state;
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getVideoName() {
+        return videoName;
+    }
+
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+}
