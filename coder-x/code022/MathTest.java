95cf808dd87db5c5efc5699ce67cfb534f32f53d
Houle <houle.cf@foxmail.com>
2022-04-25 21:40:43 +0800

    Java Basics 2022/04/25

--- coder-x/code022/MathTest.java
+++ coder-x/code022/MathTest.java
@@ -17,6 +17,20 @@ public class MathTest {
             System.out.println(Math.round(d));
             // 其他略
         }
+
+        System.out.println(Math.round(2.3));  // 2
+        System.out.println(Math.round(2.5));  // 3
+        System.out.println(Math.round(2.8));  // 3
+        System.out.println(Math.round(-2.3));  // -2
+        System.out.println(Math.round(-2.5));  // -2
+        System.out.println(Math.round(-2.8));  // -3
+
+        System.out.println(Math.rint(2.3));  // 2.0
+        System.out.println(Math.rint(2.5));  // 2.0
+        System.out.println(Math.rint(2.8));  // 3.0
+        System.out.println(Math.rint(-2.3));  // -2.0
+        System.out.println(Math.rint(-2.5));  // -2.0
+        System.out.println(Math.rint(-2.8));  // -3.0
     }
 
 }
